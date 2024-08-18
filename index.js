d08144c7a88278ea0ea605ae362a973bf9720328
Dylan Tin <dylant711@gmail.com>
2024-08-17 21:11:13 -0400

    completed arithmetic lab

---
+++ index.js
@@ -0,0 +1,72 @@
+function add() {
+    
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(a, b) {
+    return "something";
+  }
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return "something";
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return "something";
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return "something";
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+number = n;
+function increment(n) {
+    return (n + 1);
+
+}
+
+console.log(increment(n))
+
+number = n;
+function decrement(n) {
+    return (n - 1);
+
+}
+
+console.log(decrement(n)); 
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
+    
